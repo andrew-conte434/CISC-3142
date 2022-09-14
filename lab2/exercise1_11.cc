f3f11a74f4472e23f32a47be852d0e15d9d2d428
andrew-conte434 <conteandrew5@gmail.com>
2022-09-14 19:49:04 -0400

    Lab 2

---
+++ lab2/exercise1_11.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "Running program Ex1.11..." << std::endl;
+	std::cout << "Please enter two numbers...";
+	int x = 0, y = 0;
+	std::cin  >> x >> y;
+	int larger = 0, smaller = 0;
+	if(x >= y){
+		larger = x;
+		smaller = y;
+	} else {
+		larger = y;
+		smaller = x;
+	}
+	std::cout<<"The values between " << x << " and " << y << " are:"
+		<<std::endl;
+	for(int i = smaller; i < larger; i++){
+		std::cout << i << " ";
+	}
+	std::cout<<std::endl;
+	std::cout << "And now with a while loop..." << std::endl;
+	int value = smaller;
+	while(value < larger) {
+		std::cout << value << " ";
+		value++;
+	}
+	std::cout<<std::endl;
+	return 0;
+}
---
+++ lab2/exercise1_5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Running program Ex1.5..." << endl;
+	cout<< "Enter two numbers: "<<endl;
+	int x = 0, y = 0;
+	cin >> x >> y;
+	cout << "The sum of ";
+	cout << x;
+	cout << " and ";
+	cout << y;
+	cout << " is ";
+	cout << x + y;
+	cout << endl;
+
+	return 0;
+}
